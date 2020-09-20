3e54fd03017a2a14867da84b08f191ec5b19f318
chiragdoctor <chirag.doctor@gmail.com>
2020-09-20 18:50:28 +0530

    Updates

---
+++ precache-manifest.092d934340b5b443c3d2dc773084600b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba4c81109a3f6b27dce7a6fac4fe6ffe",
+    "url": "/monsotor-rolodox/index.html"
+  },
+  {
+    "revision": "1dccb650532b48375cda",
+    "url": "/monsotor-rolodox/static/css/main.09a10777.chunk.css"
+  },
+  {
+    "revision": "2348cf9be702e2f66f64",
+    "url": "/monsotor-rolodox/static/js/2.38a10f14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsotor-rolodox/static/js/2.38a10f14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1dccb650532b48375cda",
+    "url": "/monsotor-rolodox/static/js/main.d08a842d.chunk.js"
+  },
+  {
+    "revision": "cf52d706a528f7c21635",
+    "url": "/monsotor-rolodox/static/js/runtime-main.a74067d9.js"
+  }
+]);
